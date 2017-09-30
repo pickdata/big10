adf921ce9e034ce97a5f9d63747bc1ef68018e90
mindo <jeehyun.rek@gmail.com>
2017-10-01 02:48:21 +0900

    first commit

---
+++ src/main/java/com/example/big10/Big10Application.java
@@ -0,0 +1,12 @@
+package com.example.big10;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Big10Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Big10Application.class, args);
+	}
+}
